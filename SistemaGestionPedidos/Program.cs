02d2dad501ac8780000a9fa66d7908d21d75d14d
AxelDev19 <axelenriquecerritos@gmail.com>
2026-07-22 09:28:00 -0600

    Inicio del proyecto

---
+++ SistemaGestionPedidos/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.IO;
+
+namespace SistemaGestionPedidos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool menuCorriendo = true;
+
+            while(menuCorriendo)
+            {
+                Console.WriteLine("\n-------------------------------------------------------------------------------");
+                Console.WriteLine("SISTEMA DE GESTIÓN DE PEDIDOS");
+                Console.WriteLine("-------------------------------------------------------------------------------");
+                Console.WriteLine("1.Registrar pedido");
+                Console.WriteLine("2.Mostrar todos los pedidos");
+                Console.WriteLine("3.Buscar pedido");
+                Console.WriteLine("4.Modificar pedido");
+                Console.WriteLine("5.Cambiar estado");
+                Console.WriteLine("6.Eliminar pedido");
+                Console.WriteLine("7.Filtrar pedidos");
+                Console.WriteLine("8.Mostrar estadísticas");
+                Console.WriteLine("9.Mostrar ranking de pedidos");
+                Console.WriteLine("10.Salir\n");
+
+                try
+                {
+                    Console.Write("Escoja una opción: ");
+                    int opcion = int.Parse(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            break;
+                        
+                        case 2:
+                            break;
+
+                        case 3:
+                            break;
+
+                        case 4:
+                            break;
+
+                        case 5:
+                            break;
+
+                        case 6:
+                            break;
+
+                        case 7:
+                            break;
+
+                        case 8:
+                            break;
+
+                        case 9:
+                            break;
+
+                        case 10:
+                            Console.WriteLine("\nGRACIAS VUELVA PRONTO");
+                            menuCorriendo = false;
+                            break;
+
+                        default:
+                            Console.WriteLine("\nOpción no válida");
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\nFormato no válido");
+                }
+                
+            }
+        }
+    }
+}
